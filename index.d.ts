b5ed6c524af73145e680ff2f9af796becffa9332
Erik Pukinskis <erik@snowedin.net>
2022-01-23 09:20:49 -0700

    Declare default not named export

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { GraphQLFormattedError, DocumentNode } from 'graphql'
 
-export declare const GraphQLErrorHandler = (
+export declare default = (
   error: GraphQLFormattedError,
   query: string | DocumentNode,
   filename?: string) => never

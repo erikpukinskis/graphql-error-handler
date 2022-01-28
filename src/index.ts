e82297ada6e6c5530fef697ffcf160105e0a2e2c
Erik Pukinskis <erik@snowedin.net>
2022-01-27 21:17:04 -0700

    Export errorLink

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { handleResult } from './handleResult'
 export { handleLoaderError } from './handleLoaderError'
 export { throwError } from './throwError'
+export { errorLink } from './errorLink'

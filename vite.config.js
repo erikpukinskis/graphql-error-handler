d8bd10ee901d93ca6907f4063c02d7b3dc9176e6
Erik Pukinskis <erik@snowedin.net>
2022-01-27 22:00:00 -0700

    WIP Vite

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'graphql-error-handler',
+      fileName: (format) => `index.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['graphql', '@apollo/client'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'graphql': 'GraphQL',
+          '@apollo/client': 'ApolloClient',
+        },
+      },
+    },
+    sourcemap: true,
+  },
+})
